32f72a66993e6791d62ce4142e12ffdec18d2284
Tommi Siik <tsiika9@gmail.com>
2024-06-22 15:56:08 +0300

    Page structure created according to the API design.

---
+++ pages/admin/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>This is admin page.</p>
+        <p>You can't access here without logging in.</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/concerts/[id].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>This is single concert page.</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
