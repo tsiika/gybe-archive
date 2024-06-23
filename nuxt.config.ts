a52c8f5393d72bb34e203446f13cb839a0c97591
Tommi Siik <tsiika9@gmail.com>
2024-06-23 13:52:25 +0300

    Eslint added.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
+
     css: [
         '@/assets/css/main.css',
     ],
+
     postcss: {
         plugins: {
             tailwindcss: {},
             autoprefixer: {},
         },
     },
+
+    modules: ["@nuxt/eslint"]
 });
